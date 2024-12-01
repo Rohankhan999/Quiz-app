c6dfadd0207535f9b05a6282a140a27aa38351f0
Rohankhan999 <rohankhan08453@gmail.com>
2024-12-01 21:29:32 +0500

    /

---
+++ app.js
@@ -0,0 +1,6 @@
+var NAME = prompt('Enter your Name!')
+document.querySelector('#prompt').innerHTML = "Hello ! " + " " + NAME.toUpperCase() + " " +  "Welcome!"
+function Html() {
+    // window.location.href = 'htmlpage.html'
+    window.open('htmlpage.html')
+}
