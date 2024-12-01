818561200ab4d354cdb17177a25e0d79a99fe689
Rohankhan999 <rohankhan08453@gmail.com>
2024-12-02 00:27:38 +0500

    .

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var NAME = prompt('Enter your Name!')
-document.querySelector('#prompt').innerHTML = "Hello ! " + " " + NAME.toUpperCase() + " " +  "Welcome!"
+document.querySelector('#prompt').innerHTML = "Hello " + " " + NAME.toUpperCase() + " " +  "Welcome!"
 function Html() {
     // window.location.href = 'htmlpage.html'
     window.open('htmlpage.html')
 }
+var Quiz = [
+    {
+        question : "abc?",
+        answer : ["a","b","c","d"],
+        correctAnswer : "b"
+    },
+    {
+        question : "abc?",
+        answer : ["a","b","c","d"],
+        correctAnswer : "b"
+    },
+]
+Quiz.innerHTML = `<h1>${Quiz}<h1>` +
+"<br>"
+
